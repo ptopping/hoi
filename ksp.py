580695ed0d35e02f55d65a140a37de31c6d80e53
Patrick Topping <ptopping@gmail.com>
2019-06-11 12:40:44 -0600

    Began adding parts

--- ksp.py
+++ ksp.py
@@ -3,6 +3,195 @@ import numpy as np
 from math import sqrt
 from math import pi
 
+class PART(object):
+    """docstring for PART"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+        self.modules = {}
+        self.resources = {}
+        self.models = {}
+
+    def addMODULE(self, mod):
+        self.modules.update(mod.__dict__)
+
+    def addRESOURCE(self,resource):
+        self.resources.update(resource.__dict__)
+
+    def addMODEL(self,model):
+        self.models.update(model.__dict__)
+
+class MODULE(object):
+    """docstring for MODULE"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+        self.variants = {}
+        self.fxlooks = {}
+
+    def addVARIANT(self, var):
+        self.variants.update(var.__dict__)
+
+    def addCONSTRAINLOOKFX(self, looks):
+        self.fxlooks.update(looks.__dict__)
+        
+class VARIANT(object):
+    """docstring for VARIANT"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+        self.textures = {}
+    
+    def addTEXTURE(self, txt):
+        self.textures.update(txt.__dict__)
+
+class TEXTURE(object):
+    """docstring for TEXTURE"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+
+class CONSTRAINLOOKFX(object):
+    """docstring for CONSTRAINLOOKFX"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+
+class RESOURCE(object):
+    """docstring for RESOURCE"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs                        
+
+class MODEL(object):
+    """docstring for MODEL"""
+    def __init__(self, **kwargs):
+        self.attributes = kwargs
+                        
+BlackAndWhite = VARIANT(name = 'BlackAndWhite', displayName = '#autoLOC_8007122', themeName = 'BlackAndWhite', primaryColor = '#ffffff', secondaryColor = '#000000')
+BlackAndWhite.addTEXTURE(TEXTURE(mainTextureURL = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_diffuse_O',
+_BumpMap = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_normal_O'))
+Dark = VARIANT(name = 'Dark', displayName = '#autoLOC_8007117', themeName = 'Dark', primaryColor = '#4c4f47')
+Dark.addTEXTURE(TEXTURE(mainTextureURL = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_diffuse',
+_BumpMap = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_normal'))
+White = VARIANT(name = 'White', displayName = '#autoLOC_8007119', themeName = 'White', primaryColor = '#ffffff', secondaryColor = '#ffffff')
+White.addTEXTURE(TEXTURE(mainTextureURL = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_diffuse_W',
+_BumpMap = 'Squad/Parts/Utility/rockomaxAdapters/Assets/Rockomax_Adapters_normal'))
+ModulePartVariants = MODULE(name = 'ModulePartVariants', baseVariant = 'BlackAndWhite', useMultipleDragCubes = False)
+ModulePartVariants.addVARIANT(BlackAndWhite)
+ModulePartVariants.addVARIANT(Dark)
+ModulePartVariants.addVARIANT(White)
+noseCone = PART(name = 'noseCone', module = 'Part', author = 'AlexanderM', mesh = 'aerodynamicNoseCone.mu', scale = 1.0, rescaleFactor = 1, 
+node_stack_bottom01 = '0.0, 0.0, 0.0, 0.0, -1.0, 0.0', CenterOfDisplacement = '0.0, 0.2, 0.0', TechRequired = 'stability', entryCost = 2000, cost = 240, category = 'Aero', subcategory = 0,
+title = 'Aerodynamic Nose Cone', manufacturer = 'Goliath National Products',
+description = 'Aerodynamic, lightweight and mostly non-explosive. As a reminder to all personnel operating nearby, this part is really sharp and itd probably hurt if you fell on it.',
+attachRules = '1,0,1,1,0', stackSymmetry = 2, mass = 0.03, dragModelType = 'default', maximum_drag = 0.1, minimum_drag = 0.1, angularDrag = 0.5, crashTolerance = 10, maxTemp = 2400,
+emissiveConstant = 0.7, thermalMassModifier = 6.0, fuelCrossFeed = False, bulkheadProfiles = 'size1', tags = 'aero aircraft booster )cap drag fligh plane rocket speed stab stream')
+noseCone.addMODULE(ModulePartVariants)
+
+ModuleAeroSurface = MODULE(name = 'ModuleAeroSurface', useInternalDragModel = True, dragCoeff = 0.6, deflectionLiftCoeff = 0.38, ctrlSurfaceRange = 70, ctrlRangeFactor = 0.2,
+ctrlSurfaceArea = 1, actuatorSpeed = 20, transformName = 'Flap', defaultActionGroup = 'Brakes', liftingSurfaceCurve = 'SpeedBrake', ignorePitch = True, ignoreYaw = True, uncasedTemp = 1200,
+casedTemp = 2400)
+FXModuleLookAtConstraint = MODULE(name = 'FXModuleLookAtConstraint')
+FXModuleLookAtConstraint.addCONSTRAINLOOKFX(CONSTRAINLOOKFX(targetName = 'PistonArm', rotatorsName = 'PistonBase'))
+FXModuleLookAtConstraint.addCONSTRAINLOOKFX(CONSTRAINLOOKFX(targetName = 'PistonBase', rotatorsName = 'PistonArm'))
+airbrake1 = PART(name = 'airbrake1', module = 'Part', author = 'Porkjet', mesh = 'Airbrake.mu', rescaleFactor = 1, node_attach = '0.0, 0.0, -0.025, 0.0, 0.0, 1.0, 1',
+CoMOffset = '0.0, -0.2, 0.2', CoPOffset = '0.0, -0.2, 0.2', CoLOffset = '0.0, -0.2, 0.2', TechRequired = 'advAerodynamics', entryCost = 20000, cost = 1000, category = 'Aero', subcategory = 0,
+title = 'A.I.R.B.R.A.K.E.S', manufacturer = 'C7 Aerospace Division',
+description = 'Research into feasible ways to slow down a plane in mid-air showed that loose hull panels are pretty great as drag-inducing deceleration devices. The discovery led to the invention of the Aerodynamically Integrated Retrograde Braking Robustly Armed Kinetic Extending System (A.I.R.B.R.A.K.E.S.), which is now popular mostly as a cheap alternative to longer runways. C7 Engineers are still trying to come up with a shorter name for such a long acronym.',
+attachRules = '0,1,0,0,1', mass = 0.05, thermalMassModifier = 5.0, emissiveConstant = 0.4, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02, angularDrag = 2,
+crashTolerance = 8, maxTemp = 2400, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf',
+tags = '(air airbrake aircraft brake dive drag fligh landing plane slow speed spoil')
+airbrake1.addMODULE(ModuleAeroSurface)
+airbrake1.addMODULE(FXModuleLookAtConstraint)
+
+ModuleResourceIntake = MODULE(name = 'ModuleResourceIntake', resourceName = 'IntakeAir', checkForOxygen = True, area = 0.0031, intakeSpeed = 15, intakeTransformName = 'Intake',
+machCurve = {'key' : '1 1 0 0', 'key' : '1.5 0.9 -0.4312553 -0.4312553', 'key' : '2.5 0.45 -0.5275364 -0.5275364', 'key' : '3.5 0.1 0 0'})
+IntakeAir = RESOURCE(name = 'IntakeAir', amount = 2, maxAmount = 2)
+airScoop = PART(name = 'airScoop', module = 'Part', author = 'NovaSilisko, Porkjet', mesh = 'RadialIntake.mu', rescaleFactor = 1, node_attach = '0.0, 0.3370661, 0.0, 0.0, 1.0, 0.0',
+buoyancy = 0.1, TechRequired = 'aerodynamicSystems', entryCost = 2500, cost = 250, category = 'Aero', subcategory = 0, title = 'XM-G50 Radial Air Intake',
+manufacturer = 'Vac-Co Advanced Suction Systems',
+description = 'An intake duct version that can be mounted at the sides of a fuselage. Warranty does not cover engine flame-outs or any objects, inanimate or otherwise, sucked in by the intake. Optimized for subsonic flight.',
+attachRules = '0,1,0,0,0', mass = 0.02, thermalMassModifier = 6.0, heatConductivity = 0.06, dragModelType = 'default', maximum_drag = 0.2, minimum_drag = 0.2, angularDrag = 1,
+crashTolerance = 10, breakingForce = 200, breakingTorque = 200, maxTemp = 2000, bulkheadProfiles = 'srf', tags = 'aero aircraft breathe fligh inlet jet oxygen plane subsonic suck')
+airScoop.addMODULE(ModuleResourceIntake)
+airScoop.addRESOURCE(IntakeAir)
+
+ModuleControlSurface = MODULE(name = 'ModuleControlSurface', useInternalDragModel = True, dragCoeff = 0.5, deflectionLiftCoeff = 0.86, ctrlSurfaceRange = 15, ctrlSurfaceArea = 1,
+actuatorSpeed = 25, transformName = 'ControlSurface')
+airlinerCtrlSrf = PART(name = 'airlinerCtrlSrf', module = 'Part', author = 'Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, 1.0, 0.0, 0.0', CoMOffset = '0, -0.3730053, 0',
+CoLOffset = '0, -0.3730053, 0', CoPOffset = '0, -0.3730053, 0', TechRequired = 'heavyAerodynamics', entryCost = 16400, cost = 800, category = 'Aero', subcategory = 0,
+title = 'FAT-455 Aeroplane Control Surface', manufacturer = 'WinterOwl Aircraft Emporium', description = 'Large conventional control surface.', attachRules = '0,1,0,1,1', mass = 0.17,
+thermalMassModifier = 6, heatConductivity = 0.12, emissiveConstant = 0.4, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02, angularDrag = 2, crashTolerance = 15,
+maxTemp = 1200, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf', tags = 'aileron aircraft (elev flap fligh liner plane )rudder spoil stab')
+airlinerCtrlSrf.addMODULE(ModuleControlSurface)
+airlinerCtrlSrf.addMODEL(MODEL(model = 'Squad/Parts/Aero/airlinerWings/ControlSurface'))
+
+ModuleLiftingSurface = MODULE(name = 'ModuleLiftingSurface', useInternalDragModel = True, deflectionLiftCoeff = 7.8, dragAtMaxAoA = 0.6, dragAtMinAoA = 0.0)
+LiquidFuel = RESOURCE(name = 'LiquidFuel', amount = 0, maxAmount = 600)
+airlinerMainWing = PART(name = 'airlinerMainWing', module = 'Part', author = 'Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1', CoMOffset = '-2.5, 0.25, 0',
+CoLOffset = '-2.5, 0.25, 0', CoPOffset = '-2.5, 0.25, 0', TechRequired = 'heavyAerodynamics', entryCost = 36400, cost = 2800, category = 'Aero', subcategory = 0,
+title = 'FAT-455 Aeroplane Main Wing', manufacturer = 'WinterOwl Aircraft Emporium',
+description = 'One of the largest lifting surfaces in production. The internal volume of these is big enough to carry generously sized fuel tanks.',
+attachRules = '0,1,0,1,1', mass = 0.78, thermalMassModifier = 3, heatConductivity = 0.12, emissiveConstant = 0.4, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02,
+angularDrag = 2, crashTolerance = 15, maxTemp = 1200, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf', 
+tags = 'aero aircraft airlin fligh foil fuel ?lf lift liquid swept wet')
+airlinerMainWing.addMODULE(ModuleLiftingSurface)
+airlinerMainWing.addRESOURCE(LiquidFuel)
+airlinerMainWing.addMODEL(MODEL(model = 'Squad/Parts/Aero/airlinerWings/MainWing')) 
+
+ModuleControlSurface = MODULE(name = 'ModuleControlSurface', useInternalDragModel = True, dragCoeff = 0.6, deflectionLiftCoeff = 2.69, ctrlSurfaceRange = 15, ctrlSurfaceArea = 0.37,
+actuatorSpeed = 25, transformName = 'ctrlSrf')
+FlagDecal = MODULE(name = 'FlagDecal', textureQuadName = 'FLAG')
+airlinerTailFin = PART(name = 'airlinerTailFin', module = 'Part', author = 'Porkjet', rescaleFactor = 1, node_attach = '0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1', CoLOffset = '-2.0, 0.4, 0',
+CoPOffset = '-2.0, 0.4, 0', CoMOffset = '-2.0, 0.4, 0', TechRequired = 'heavyAerodynamics', entryCost = 4000, cost = 1000, category = 'Aero', subcategory = 0,
+title = 'FAT-455 Aeroplane Tail Fin', manufacturer = 'WinterOwl Aircraft Emporium', description = 'Large conventional wing with built-in control surface.', attachRules = '0,1,0,1,1',
+mass = 0.36, thermalMassModifier = 3.0, heatConductivity = 0.12, emissiveConstant = 0.4, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02, angularDrag = 2,
+crashTolerance = 15, maxTemp = 1200, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf',
+tags = 'aileron aircraft control (elev fligh foil lift liner )rudder stab swept wing')
+airlinerTailFin.addMODULE(ModuleControlSurface)
+airlinerTailFin.addMODULE(FlagDecal)
+airlinerTailFin.addMODEL(MODEL(model = 'Squad/Parts/Aero/airlinerWings/TailFin'))
+
+ModuleControlSurface = MODULE(name = 'ModuleControlSurface', useInternalDragModel = True, dragCoeff = 0.5, deflectionLiftCoeff = 0.4, ctrlSurfaceRange = 10, ctrlSurfaceArea = 1,
+actuatorSpeed = 35, transformName = 'ctrlSrf')
+AdvancedCanard = PART(name = 'AdvancedCanard', module = 'Part', author = 'C. Jenkins, Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1',
+CoMOffset = '-0.4, -0.05, 0', CoLOffset = '-0.4, -0.05, 0', CoPOffset = '-0.4, -0.05, 0', TechRequired = 'hypersonicFlight', entryCost = 9200, cost = 800, category = 'Aero',
+subcategory = 0, title = 'Advanced Canard', manufacturer = 'C7 Aerospace Division',
+description = 'Our engineers thought this design looked "high tech" and therefore must be clear improvement on earlier models.', attachRules = '0,1,0,1,0', mass = 0.08,
+thermalMassModifier = 8.0, heatConductivity = 0.06, emissiveConstant = 0.95, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02, angularDrag = 3, crashTolerance = 12,
+maxTemp = 2400, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf', tags = 'aero (air control (elev fighter fligh (fore lift plane )rudder stab swept tail')
+AdvancedCanard.addMODULE(ModuleControlSurface)
+AdvancedCanard.addMODEL(MODEL(model = 'Squad/Parts/Aero/airplaneFins/AdvCanard'))
+
+ModuleControlSurface = MODULE(name = 'ModuleControlSurface', useInternalDragModel = True, dragCoeff = 0.5, deflectionLiftCoeff = 0.52, ctrlSurfaceRange = 15, ctrlSurfaceArea = 1,
+actuatorSpeed = 30, transformName = 'ctrlSrf')
+CanardController = PART(name = 'CanardController', module = 'Part', author = 'C. Jenkins, Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1',
+CoMOffset = '-0.55, -0.15, 0', CoLOffset = '-0.55, -0.15, 0', CoPOffset = '-0.55, -0.15, 0', TechRequired = 'advAerodynamics', entryCost = 11400, cost = 720, category = 'Aero',
+subcategory = 0, title = 'Standard Canard', manufacturer = 'C7 Aerospace Division',
+description = 'Our engineers had a stroke of inspiration after "visiting" the Kerlington production facilities. Introducing our new controllable canards. Warning, hard maneuvering may cause unintended stage separation.',
+attachRules = '0,1,0,1,0', mass = 0.1, thermalMassModifier = 8.0, heatConductivity = 0.06, emissiveConstant = 0.95, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02,
+angularDrag = 3, crashTolerance = 12, maxTemp = 2400, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf',
+tags = 'aero (air control (elev fligh (fore lift plane )rudder stab swept tail')
+CanardController.addMODULE(ModuleControlSurface)
+CanardController.addMODEL(MODEL(model = 'Squad/Parts/Aero/airplaneFins/Canard'))
+
+ModuleLiftingSurface = MODULE(name = 'ModuleLiftingSurface', useInternalDragModel = True, deflectionLiftCoeff = 1.37, dragAtMaxAoA = 0.6, dragAtMinAoA = 0.1)
+sweptWing = PART(name = 'sweptWing', module = 'Part', author = 'C. Jenkins, Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1', CoMOffset = '1.75,-1.0, 0',
+CoLOffset = '1.75,-1.0, 0', CoPOffset = '1.75,-1.0, 0', TechRequired = 'aviation', entryCost = 3600, cost = 620, category = 'Aero', subcategory = 0, title = 'Swept Wings',
+manufacturer = 'WinterOwl Aircraft Emporium',
+description = 'A prototype swept back wing, part of our "Lets Fly", line. Made of light weight composite materials. Guaranteed to generate lift, not guaranteed to ensure crew safety.',
+attachRules = '0,1,0,1,1', mass = 0.275, thermalMassModifier = 8.0, heatConductivity = 0.06, emissiveConstant = 0.95, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02,
+angularDrag = 2, crashTolerance = 15, maxTemp = 2400, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf', tags = 'aero (air fligh lift plane')
+sweptWing.addMODULE(ModuleControlSurface)   
+sweptWing.addMODEL(MODEL(model = 'Squad/Parts/Aero/airplaneFins/Swept'))
+
+ModuleControlSurface = MODULE(name = 'ModuleControlSurface', useInternalDragModel = True, dragCoeff = 0.6, deflectionLiftCoeff = 0.61, ctrlSurfaceRange = 25, ctrlSurfaceArea = 1,
+actuatorSpeed = 25, transformName = 'ctrlSrf')
+tailfin = PART(name = 'tailfin', module = 'Part', author = 'C. Jenkins, Porkjet', rescaleFactor = 1, node_attach = '0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1', CoMOffset = '0.75, -0.5, 0',
+CoLOffset = '0.75, -0.5, 0', CoPOffset = '0.75, -0.5, 0', TechRequired = 'aviation', entryCost = 3800, cost = 600, category = 'Aero', subcategory = 0, title = 'Tail Fin',
+manufacturer = 'C7 Aerospace Division',
+description = 'This tailfin has passed extensive modeling in our patented P.A.S system.(Paper Airplane Simulation). This fin has been blunted to prevent accidental dismemberment of installing technicians.',
+attachRules = '0,1,0,1,0', mass = 0.125, thermalMassModifier = 8.0, heatConductivity = 0.06, emissiveConstant = 0.95, dragModelType = 'none', maximum_drag = 0.02, minimum_drag = 0.02,
+angularDrag = 2, crashTolerance = 12, maxTemp = 2400, explosionPotential = 0.1, fuelCrossFeed = True, bulkheadProfiles = 'srf',
+tags = 'aero (air canard control (elev fligh (fore lift plane )rudder stab swept')
+tailfin.addMODULE(ModuleControlSurface)
+tailfin.addMODEL(MODEL(model = 'Squad/Parts/Aero/airplaneFins/TailFin'))
+
 class CelestialBody(object):
     """docstring for ."""
     def __init__(self,name,type,biomes,sm_axis,radius,GM,rot_per,SOI,atmo_border,atmo_limit,space_border):
