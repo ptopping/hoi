f42f27ddaa7ed0617bcda54cb01fb0c6fafa6ec3
ptopping <ptopping@gmail.com>
2019-08-26 19:16:31 -0600

    Create la.py

---
+++ la.py
@@ -0,0 +1,10 @@
+index : The unique ID of each observation.
+class : The true class of each observation. The classes are binary (0 or 1).
+predicted_prob : The model's estimate of probability that the observation belongs to class 1.
+Instructions
+Using the model_outcome.csv, develop either an R or Python script to:
+
+Manually calculate the sensitivity and specificity of the model, using a predicted_prob threshold of greater than or equal to .5.
+Manually calculate the Area Under the Receiver Operating Characteristic Curve.
+Visualize the Receiver Operating Characterstic Curve.
+Email your finalized, completely reproducible .r or .py script to L.A. Care's Talent Acquisition Specialist, Jamessa Jones, at jjones@lacare.org.
